14ec3bfce61a980fa4af8b02162d68ac6d4ff440
Washington Botelho <wbotelhos@gmail.com>
2011-10-16 18:56:34 -0200

    Best pratice for plugin build.

--- js/jquery.plugin.js
+++ js/jquery.plugin.js
@@ -1,13 +1,13 @@
-/**
- * jQuery Plugin - A Plugin - http://wbotelhos.com/plugin
+/*!
+ * jQuery ${NAME} - ${NAME} - http://wbotelhos.com/${NAME_LOWER}
  * ---------------------------------------------------------------------------------
  *
- * jQuery Plugin is a plugin that  automatically.
+ * jQuery ${NAME} ${FULL_NAME}
  *
  * Licensed under The MIT License
  *
  * @version			0.1.0
- * @since			xx.xx.2011
+ * @since			${CREATED_AT}
  * @author			Washington Botelho dos Santos
  * @documentation	http://wbotelhos.com/plugin
  * @twitter			http://twitter.com/wbotelhos
@@ -16,98 +16,50 @@
  *
  * Usage with default values:
  * ---------------------------------------------------------------------------------
- * $('#plugin').plugin();
+ * $('#${NAME_LOWER}').${NAME_LOWER}();
  *
- * <div id="plugin"></div>
+ * <div id="${NAME_LOWER}"></div>
  *
- * $('.plugin').plugin();
+ * $('.${NAME_LOWER}').${NAME_LOWER}();
  *
- * <div class="plugin"></div>
- * <div class="plugin"></div>
- * <div class="plugin"></div>
+ * <div class="${NAME_LOWER}"></div>
+ * <div class="${NAME_LOWER}"></div>
+ * <div class="${NAME_LOWER}"></div>
  *
  */
 
 ;(function($) {
 
-	$.fn.plugin = function(settings) {
-
-		if (this.length == 0) {
-			debug('Selector invalid or missing!');
-			return;
-		} else if (this.length > 1) {
+	var methods = {
+		init: function(options) {
 			return this.each(function() {
-				$.fn.plugin.apply($(this), [settings]);
+
+				var $this = $(this);
+
+				var opt				= $.extend({}, $.fn.NAME_LOWER.defaults, options),
+					id				= $this.data('options', opt).attr('id');
+
+				
 			});
-		}
-
-		var opt			= $.extend({}, $.fn.plugin.defaults, settings),
-			$this		= $(this),
-			id			= this.attr('id');
-
-		if (id === undefined) {
-			id = 'plugin-' + $this.index();
-			$this.attr('id', id); 
-		}
-
-		
-
-		return $this;
-	};
-	
-	$.fn.plugin.method = function(value, idOrClass) {
-		var context = getContext(value, idOrClass, 'method'),
-			options = $(idOrClass).data('options');
-
-		if (idOrClass.indexOf('.') >= 0) {
-			return;
-		}
-
-		
-
-		return $.fn.plugin;
-	};
-
-	function getContext(value, idOrClass, name) {
-		var context = undefined;
-
-		if (idOrClass == undefined) {
-			debug('Specify an ID or class to be the target of the action.');
-			return;
-		}
-
-		if (idOrClass) {
-			if (idOrClass.indexOf('.') >= 0) {
-				var idEach;
-
-				return $(idOrClass).each(function() {
-					idEach = '#' + $(this).attr('id');
-
-					if (name == 'method') {
-						$.fn.plugin.method(value, idEach);
-					}
-				});
-			}
-
-			context = $(idOrClass);
-
-			if (!context.length) {
-				debug('"' + idOrClass + '" is a invalid identifier for the public funtion $.fn.plugin.' + name + '().');
-				return;
+		}, debug: function(message) {
+			if (window.console && window.console.log) {
+				window.console.log(message);
 			}
 		}
-
-		return context;
 	};
 
-	function debug(message) {
-		if (window.console && window.console.log) {
-			window.console.log(message);
-		}
+	$.fn.NAME_LOWER = function(method) {
+		if (methods[method]) {
+			return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+		} else if (typeof method === 'object' || !method) {
+			return methods.init.apply(this, arguments);
+		} else {
+			$.error('Method ' + method + ' does not exist!');
+		} 
 	};
 
-	$.fn.plugin.defaults = {
-		attribute:	null
+	$.fn.NAME_LOWER.defaults = {
+		
 	};
 
 })(jQuery);
