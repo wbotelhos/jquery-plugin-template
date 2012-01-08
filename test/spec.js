a27932f85007f9fa2067fdbc28783954290f6ab5
Washington Botelho <wbotelhos@gmail.com>
2012-01-08 14:52:36 -0200

    Improved the spec template.

--- test/spec.js
+++ test/spec.js
@@ -1,26 +1,47 @@
-describe('${NAME}', function() {
+describe('Using ID', function() {
 
 	beforeEach(function() {
-	    $('body').append('<div id="NAME_LOWER"></div>');
+	    $('body').append('<div id="${NAME_LOWER}"></div>');
 	});
 
-	afterEach(function () {
-		$('#NAME_LOWER').remove();
+	afterEach(function() {
+		$('#${NAME_LOWER}').remove();
 	});
 
-	describe('using ...', function() {
+	it ('xpto should xpto', function() {
+		// given
+		var $${NAME_LOWER} = $('#${NAME_LOWER}');
 
-		it('should be true', function() {
-			// given
-			var $NAME_LOWER = $('#NAME_LOWER');
+		// when
+		$${NAME_LOWER}.;
 
-			// when
-			$NAME_LOWER;
+		// then
+	    expect($${NAME_LOWER}).toBetruthy();
+	});
 
-			// then
-		    expect($NAME_LOWER).toBetruthy();
-		});
+});
 
+describe('Using class', function() {
+
+	beforeEach(function() {
+	    $('body').append('<div class="${NAME_LOWER}"></div><div class="${NAME_LOWER}"></div><div class="${NAME_LOWER}"></div>');
+	});
+
+	afterEach(function() {
+		$('.${NAME_LOWER}').remove();
+	});
+
+	it ('xpto should xpto', function() {
+		// given
+		var $${NAME_LOWER}s = $('.${NAME_LOWER}');
+
+		// when
+		$${NAME_LOWER}s.;
+
+		// then
+	    expect($${NAME_LOWER}s.eq(0)).toBetruthy();
+	    expect($${NAME_LOWER}s.eq(1)).toBetruthy();
+	    expect($${NAME_LOWER}s.eq(2)).toBetruthy();
 	});
 
 });
